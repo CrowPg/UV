7675055f22b8d93aee9563c80b09d16cf94d5d9f
johan <crow13pg@aol.fr>
2012-02-27 14:54:52 +0100

    + ‘P12/LO21/TP01/main_2.cpp’ / ‘P12/LO21/TP01/TP01.xcodeproj/project.pbxproj’ / ‘P12/LO21/TP01/TP01.xcodeproj/project.xcworkspace/xcuserdata/johan.xcuserdatad/UserInterfaceState.xcuserstate’ - ‘P12/LO21/TP01/TP01/Header.h’

--- P12/LO21/TP01/main_2.cpp
+++ P12/LO21/TP01/main_2.cpp
@@ -1,14 +1,9 @@
 //
-//  Header.h
+//  main_2.cpp
 //  TP01
 //
 //  Created by Johan on 27/02/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#ifndef TP01_Header_h
-#define TP01_Header_h
-
-
-
-#endif
+#include <iostream>
