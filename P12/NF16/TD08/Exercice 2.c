81e96575ede25c80073daa8b8b51d86c198ac5c8
johan <crow13pg@aol.fr>
2012-04-25 18:32:49 +0200

    / ‘P12/NF16/TD08/Exercice 2.c’

--- P12/NF16/TD08/Exercice 2.c
+++ P12/NF16/TD08/Exercice 2.c
@@ -3,3 +3,19 @@ typedef struct noeud{
 	struct noeud* gauche;
 	struct noeud* droite;
 }noeud;
+
+typedef noeud* arbre;
+
+int max_abr(arbre A){
+	if (A->droite!=NULL)
+		return max_abr(A->droie);
+	else
+		return A->val;
+}
+
+int min_abr(arbre A){
+	while (A->gauche!=NULL){
+		A=A->gauche;
+	}
+	return A->val;
+}
